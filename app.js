41c58feef72036e23dd5e627ddb3f6eca5d1458a
Dev-Damon <suckd9111@gmail.com>
2026-06-20 11:46:20 +0900

    하이라이트 자동수집: match-16

--- app.js
+++ app.js
@@ -2717,6 +2717,7 @@
     "match-13": "https://chzzk.naver.com/video/13698656", // 브라질-모로코
     "match-14": "https://chzzk.naver.com/video/13699342", // 아이티-스코틀랜드
     "match-15": "https://chzzk.naver.com/video/13795597", // 스코틀랜드-모로코
+    "match-16": "https://chzzk.naver.com/video/13796304", // 브라질-아이티
     "match-19": "https://chzzk.naver.com/video/13682431", // 미국-파라과이
     "match-20": "https://chzzk.naver.com/video/13700637", // 호주-튀르키예
     "match-21": "https://chzzk.naver.com/video/13795010", // 미국-호주
